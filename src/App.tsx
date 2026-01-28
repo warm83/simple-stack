d197d6b0ccbca59a2799cbc6f7e23c956fcc7471
Lee Hyunmin <hhunm@naver.com>
2026-01-28 17:11:14 +0900

    NotFoundページの実装

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import TasksPage from './pages/task/task'
 import TaskDetailPage from './pages/task/detail'
 import LoginPage from './pages/login/login'
 import { TasksProvider } from './context/TasksProvider'
+import NotFoundPage from './pages/not-found/not-found'
 
 export default function App() {
   return (
@@ -11,6 +12,7 @@ export default function App() {
         <Route path="/" element={<TasksPage />} />
         <Route path="/tasks/:id" element={<TaskDetailPage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="*" element={<NotFoundPage/>}/>
       </Routes>
     </TasksProvider>
   )
---
+++ src/pages/not-found/not-found.styles.ts
@@ -0,0 +1,9 @@
+export const notFoundStyles = {
+    page: {
+    minHeight: '100vh',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    background: '#ffffff',
+  }
+}
