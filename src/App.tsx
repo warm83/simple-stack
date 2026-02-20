ee0d356736625cd436c91f150f22b3c4d11e669c
Lee Hyunmin <hhunm@naver.com>
2026-02-20 17:53:21 +0900

    機能追加: メンテナンスモード

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,12 @@ import TaskDetailPage from './pages/task/detail'
 import LoginPage from './pages/login/login'
 import { TasksProvider } from './context/TasksProvider'
 import NotFoundPage from './pages/not-found/not-found'
+import MaintenancePage from './pages/maintenance/maintenance'
 
 export default function App() {
+  const isMaintenance = import.meta.env.VITE_MAINTENANCE_MODE
+  if (isMaintenance === "true") return <MaintenancePage/>
+
   return (
     <TasksProvider>
       <Routes>
